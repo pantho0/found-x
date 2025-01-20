d95fc32fed5beda627f41bfc0554542aef9cc437
pantho0 <panthomashreky6@gmail.com>
2025-01-20 22:08:20 +0600

    custimizable for input added

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,6 +1,5 @@
-import { NextResponse } from "next/server";
-
 import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 type Role = keyof typeof roleBasedRoutes;
 
