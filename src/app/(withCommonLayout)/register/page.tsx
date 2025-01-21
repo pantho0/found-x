5ddc246184b61c5f2d6e767c822f700e5e40f481
pantho0 <panthomashreky6@gmail.com>
2025-01-21 13:59:06 +0600

    hook added for mutation

--- src/app/(withCommonLayout)/register/page.tsx
+++ src/app/(withCommonLayout)/register/page.tsx
@@ -2,31 +2,15 @@
 
 import FXForm from "@/src/components/form/FXForm";
 import FXInput from "@/src/components/form/FXInput";
-
+import { useUserRegistration } from "@/src/hooks/auth.hook";
 import registerValidationSchema from "@/src/schemas/register.schema";
-import { registerUser } from "@/src/services/AuthService";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@nextui-org/button";
-import { useMutation } from "@tanstack/react-query";
-
 import Link from "next/link";
-
 import { FieldValues, SubmitHandler } from "react-hook-form";
 
 export default function RegisterPage() {
-  const {
-    mutate: handleUserRegistration,
-    isPending,
-    data,
-    isError,
-    isSuccess,
-  } = useMutation({
-    mutationKey: ["USER_REGISTRATION"],
-    mutationFn: async (userData) => await registerUser(userData),
-  });
-
-  console.log({ isPending, isError, isSuccess, data });
+  const { mutate: handleUserRegistration, isPending } = useUserRegistration();
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     const userData = {
@@ -38,6 +22,10 @@ export default function RegisterPage() {
     console.log("Inside form user data: ", userData);
     handleUserRegistration(userData);
   };
+
+  if (isPending) {
+    //handle something
+  }
 
   return (
     <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
