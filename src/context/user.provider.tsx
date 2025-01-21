5ea30ace61427924962fcffe1d8928c4ba18e38e
pantho0 <panthomashreky6@gmail.com>
2025-01-21 20:40:31 +0600

    user context added for whole app

---
+++ src/context/user.provider.tsx
@@ -0,0 +1,30 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from "react";
+import { IUser } from "../types";
+
+const UserContext = createContext<IUserProviderValues | undefined>(undefined);
+
+interface IUserProviderValues {
+  user: IUser | null;
+  isLoading: boolean;
+  setUser: (user: IUser | null) => void;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <UserContext.Provider value={{ user, setUser, isLoading, setIsLoading }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
