f395fea7e5ccab10216ec325abf24f80d7ac31d5
pantho0 <panthomashreky6@gmail.com>
2025-01-22 13:59:31 +0600

    form dynamic field append functionalites has been added

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import FXForm from "@/src/components/form/FXForm";
 import FXInput from "@/src/components/form/FXInput";
+import { Divider } from "@heroui/divider";
 import { Button } from "@nextui-org/button";
 import {
   FieldValues,
@@ -15,13 +16,21 @@ const CreatePost = () => {
 
   const { control, handleSubmit } = methods;
 
-  const { append, remove } = useFieldArray({
+  const { fields, append, remove } = useFieldArray({
     control,
     name: "questions",
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    const postData = {
+      ...data,
+      questions: data?.questions.map((que: { value: string }) => que?.value),
+    };
+    console.log(postData);
+  };
+
+  const handleFieldAppend = () => {
+    append({ name: "questions" });
   };
 
   return (
@@ -29,6 +38,20 @@ const CreatePost = () => {
       <FormProvider {...methods}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <FXInput name="title" label="Title" />
+          <Divider className="my-5" />
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Owner verification questions</h1>
+            <Button onPress={() => handleFieldAppend()}>Append</Button>
+          </div>
+
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex items-center">
+              <FXInput name={`questions.${index}.value`} label="Question" />
+              <Button onClick={() => remove(index)}>Remove</Button>
+            </div>
+          ))}
+
+          <Divider className="my-5" />
           <Button type="submit">Submit</Button>
         </form>
       </FormProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/components/spinner.js",
+    "./node_modules/@heroui/theme/dist/components/(divider|spinner).js",
   ],
   theme: {
     extend: {
