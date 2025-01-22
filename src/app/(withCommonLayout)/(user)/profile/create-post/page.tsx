f04235208a582918b27a4d6f27f647257fa5d34b
pantho0 <panthomashreky6@gmail.com>
2025-01-22 13:27:14 +0600

    form added with provider and form field array hook

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,5 +1,39 @@
-const page = () => {
-  return <div>create post</div>;
+"use client";
+import FXForm from "@/src/components/form/FXForm";
+import FXInput from "@/src/components/form/FXInput";
+import { Button } from "@nextui-org/button";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
+
+const CreatePost = () => {
+  const methods = useForm();
+
+  const { control, handleSubmit } = methods;
+
+  const { append, remove } = useFieldArray({
+    control,
+    name: "questions",
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FXInput name="title" label="Title" />
+          <Button type="submit">Submit</Button>
+        </form>
+      </FormProvider>
+    </div>
+  );
 };
 
-export default page;
+export default CreatePost;
