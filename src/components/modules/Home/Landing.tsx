adf90a454a1e006a4943df31146e594b78045e53
pantho0 <panthomashreky6@gmail.com>
2025-01-24 19:56:05 +0600

    loaded search form with react hook form

--- src/components/modules/Home/Landing.tsx
+++ src/components/modules/Home/Landing.tsx
@@ -1,21 +1,32 @@
+"use client";
 import { Input } from "@nextui-org/input";
 
 import { SearchIcon } from "../../ui/icons";
+import { useForm } from "react-hook-form";
 
 const Landing = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="bg-[url('/lostimage.jpeg')] h-screen bg-cover">
       <div className="pt-32 max-w-xl mx-auto">
-        <Input
-          aria-label="Search"
-          classNames={{
-            inputWrapper: "bg-default-100",
-            input: "text-sm",
-          }}
-          placeholder="Search"
-          size="lg"
-          startContent={<SearchIcon />}
-        />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            {...register("search")}
+            aria-label="Search"
+            classNames={{
+              inputWrapper: "bg-default-100",
+              input: "text-sm",
+            }}
+            placeholder="Search"
+            size="lg"
+            startContent={<SearchIcon />}
+          />
+        </form>
       </div>
     </div>
   );
